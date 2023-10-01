28bd857519a920aaac0fdad97752d93746ca4823
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-01 21:26:03 +0330

    Create ApplicationProperties and write Connetion to database

---
+++ src/main/java/ir/oopjdbcgeneric/util/ApplicationProperties.java
@@ -0,0 +1,9 @@
+package ir.oopjdbcgeneric.util;
+
+public class ApplicationProperties {
+
+    public final static String URL ="jdbc:postgresql://localhost:5432/postgres";
+    public final static String USERNAME ="postgres";
+    public final static String PASSWORD ="173725Hrm";
+
+}
