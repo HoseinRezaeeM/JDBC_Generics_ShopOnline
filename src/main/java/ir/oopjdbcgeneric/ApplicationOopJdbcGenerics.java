6dc9865ae30303b2373440fd43e4850fc1113618
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-02 21:40:21 +0330

    Create ApplicationOopJdbcGenerics class write main and complie

---
+++ src/main/java/ir/oopjdbcgeneric/ApplicationOopJdbcGenerics.java
@@ -0,0 +1,13 @@
+package ir.oopjdbcgeneric;
+
+import ir.oopjdbcgeneric.menu.BaseMenu;
+
+import java.sql.SQLException;
+
+public class ApplicationOopJdbcGenerics {
+    public static void main(String[] args) throws SQLException {
+        BaseMenu baseMenu =new BaseMenu();
+        baseMenu.fistMenu();
+
+    }
+}
