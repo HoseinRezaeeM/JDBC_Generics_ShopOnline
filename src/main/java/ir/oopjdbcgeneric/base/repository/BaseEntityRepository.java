d1f34e66bac7409afba862da20d4b7987b7d148f
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-02 21:35:34 +0330

    Create BaseEntityService class

--- src/main/java/ir/oopjdbcgeneric/base/repository/BaseEntityRepository.java
+++ src/main/java/ir/oopjdbcgeneric/base/repository/BaseEntityRepository.java
@@ -1,4 +1,12 @@
 package ir.oopjdbcgeneric.base.repository;
 
-public interface BaseEntityRepository {
+import ir.oopjdbcgeneric.base.domain.BaseEntity;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+
+@SuppressWarnings("unused")
+public interface BaseEntityRepository<ID extends Serializable,TYPE extends BaseEntity<ID>> {
+    void Save(TYPE entity) throws SQLException;
+
 }
--- src/main/java/ir/oopjdbcgeneric/base/service/BaseEntityService.java
+++ src/main/java/ir/oopjdbcgeneric/base/service/BaseEntityService.java
@@ -1,4 +1,10 @@
 package ir.oopjdbcgeneric.base.service;
 
-public interface BaseEntityService {
+import ir.oopjdbcgeneric.base.domain.BaseEntity;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+
+public interface BaseEntityService<ID extends Serializable ,TYPE extends BaseEntity<ID>>{
+    void Save(TYPE entity) throws SQLException;
 }
