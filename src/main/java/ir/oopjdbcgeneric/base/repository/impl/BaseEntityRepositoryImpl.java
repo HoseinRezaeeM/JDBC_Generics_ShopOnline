9b8c8e6fc159f1b0f3ae56dc7199dcdc923b2c54
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-02 21:33:54 +0330

    Create BaseEntityRepository class

--- src/main/java/ir/oopjdbcgeneric/base/repository/impl/BaseEntityRepositoryImpl.java
+++ src/main/java/ir/oopjdbcgeneric/base/repository/impl/BaseEntityRepositoryImpl.java
@@ -1,4 +1,39 @@
 package ir.oopjdbcgeneric.base.repository.impl;
 
-public class BaseEntityRepositoryImpl {
+import ir.oopjdbcgeneric.base.domain.BaseEntity;
+import ir.oopjdbcgeneric.base.repository.BaseEntityRepository;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@SuppressWarnings("unused")
+@Getter
+public abstract class BaseEntityRepositoryImpl<ID extends Serializable,TYPE extends BaseEntity<ID>> implements BaseEntityRepository<ID,TYPE> {
+
+    private final Connection connection;
+    public BaseEntityRepositoryImpl(Connection connection) {
+        this.connection=connection;
+    }
+
+    @Override
+    public void Save(TYPE entity) throws SQLException {
+        String sql ="INSERT INTO " + getTableName() + "  " + getColumnName() + " VALUES " +  getCountOfQuestionMarkForParams() ;
+       try (PreparedStatement statement =connection.prepareStatement(sql)){
+           fillParamForStatement(statement, entity, false);
+           statement.executeUpdate();
+       }catch (Exception e){
+           e.printStackTrace();
+       }
+    }
+
+    protected abstract void fillParamForStatement(PreparedStatement preparedStatement, TYPE entity, boolean isCart) throws SQLException;
+
+    protected abstract String getCountOfQuestionMarkForParams();
+
+    protected abstract String getColumnName();
+
+    protected abstract String getTableName();
 }
