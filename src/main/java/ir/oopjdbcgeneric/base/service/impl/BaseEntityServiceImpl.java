460c7fe241c4852baf7537851cd4f03a98c8a879
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-02 21:35:01 +0330

    Create BaseEntityServiceImpl class

--- src/main/java/ir/oopjdbcgeneric/base/service/impl/BaseEntityServiceImpl.java
+++ src/main/java/ir/oopjdbcgeneric/base/service/impl/BaseEntityServiceImpl.java
@@ -1,4 +1,23 @@
 package ir.oopjdbcgeneric.base.service.impl;
 
-public class BaseEntityServiceImpl {
+import ir.oopjdbcgeneric.base.domain.BaseEntity;
+import ir.oopjdbcgeneric.base.repository.BaseEntityRepository;
+import ir.oopjdbcgeneric.base.service.BaseEntityService;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+
+public class BaseEntityServiceImpl<ID extends Serializable, TYPE extends BaseEntity<ID>,
+        R extends BaseEntityRepository<ID, TYPE>> implements BaseEntityService<ID, TYPE> {
+
+    protected R repository;
+
+    public BaseEntityServiceImpl(R repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void Save(TYPE entity) throws SQLException {
+        repository.Save(entity);
+    }
 }
